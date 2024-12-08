f6f68c9558bf03319eef5afd81a686febd8e9f9e
ShiAnn Rule <shiann.oliver@gmail.com>
2024-12-08 13:14:30 -0800

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -37,9 +37,73 @@ const tripsFindByCode = async(req, res) => {
             .status(200)
             .json(q);
     }
-}
+};
+//post
+const tripsAddTrip = async(req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+    });
+    const q = await newTrip.save();
+        if(!q){
+            return res
+                .status(400)
+                .json(err);
+        }
+        else { 
+            return res 
+                .status(201)
+                .json(q);
+        }
+
+        //console.log(q);
+};
+//put
+
+const tripsUpdateTrip = async(req, res) => {
+    //console.log(req.params);
+    //console.log(req.body);
+    const q = await Model
+        .findOneAndUpdate(
+        {'code':req.params.tripCode},
+        {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+        })
+        .exec();
+
+        if(!q){
+            return res
+                .status(400)
+                .json(err);
+        }
+        else { 
+            return res 
+                .status(201)
+                .json(q);
+        }
+
+        //console.log(q);
+};
+
+
+
 
 module.exports = {
     tripsList,
-    tripsFindByCode
-}
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
+};
